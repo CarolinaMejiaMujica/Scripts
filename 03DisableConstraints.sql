23d3751c43b40d63a21026119b115b723cb11d03
CarolinaMejiaMujica <carolinae.mejiam@pucp.edu.pe>
2021-09-05 00:09:35 -0500

    actualización de comandos

--- 03DisableConstraints.sql
+++
@@ -1,8 +0,0 @@
-
-ALTER TABLE secuencias drop constraint fk_secuencias_departamento;
-
-ALTER TABLE agrupamiento drop constraint fk_agrupamiento_algoritmo;
-
-ALTER TABLE agrupamiento drop constraint fk_agrupamiento_secuencia;
-
-ALTER TABLE agrupamiento drop constraint fk_agrupamiento_variante;
