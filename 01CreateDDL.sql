bd19e3f389d6f1d798c319cc30f84c46871ece5b
CarolinaMejiaMujica <carolinae.mejiam@pucp.edu.pe>
2022-10-18 19:55:46 -0500

    datos iniciales

--- 01CreateDDL.sql
+++ 01CreateDDL.sql
@@ -8,13 +8,13 @@ SET DATESTYLE TO 'European';
 --------------TABLA DEPARTAMENTOS-----------------
 CREATE TABLE Departamentos(
   id_departamento SERIAL NOT NULL,
-  nombre VARCHAR(100) NOT NULL,
+  nombre VARCHAR(30) NOT NULL,
   latitud DOUBLE PRECISION[],
   longitud DOUBLE PRECISION[],
   CONSTRAINT pk_id_departamento PRIMARY KEY (id_departamento)
 );
 
-ALTER TABLE Departamentos OWNER TO postgres;
+ALTER TABLE Departamentos OWNER TO rlnivuldeinkhw;
 
 COMMENT ON TABLE Departamentos
 	IS 'Tabla donde se registran los departamentos';
@@ -33,12 +33,14 @@ COMMENT ON COLUMN Departamentos.longitud
 --------------TABLA SECUENCIAS-----------------
 CREATE TABLE Secuencias(
   id_secuencia SERIAL NOT NULL,
-  codigo VARCHAR(25) NOT NULL,
+  codigo VARCHAR(30) NOT NULL,
   secuencia TEXT NOT NULL,
   fecha_recoleccion DATE NOT NULL,
   secuencia_alineada TEXT,
   id_departamento INTEGER NOT NULL,
-  linaje_pango TEXT,
+  linaje_pango VARCHAR(30),
+  variante VARCHAR(30),
+  estado INTEGER,
   CONSTRAINT pk_id_secuencia PRIMARY KEY (id_secuencia)
 );
 
@@ -47,7 +49,7 @@ ALTER TABLE ONLY Secuencias
 	FOREIGN KEY (id_departamento) 
 	REFERENCES departamentos(id_departamento);
 
-ALTER TABLE Secuencias OWNER TO postgres;
+ALTER TABLE Secuencias OWNER TO rlnivuldeinkhw;
 
 COMMENT ON TABLE Secuencias
 	IS 'Tabla donde se registran las secuencias genómicas SARS-CoV-2';
@@ -71,20 +73,26 @@ COMMENT ON COLUMN Secuencias.id_departamento
     IS 'Identificador del departamento donde se recolecto la secuencia genómica SARS-CoV-2';
 	
 COMMENT ON COLUMN Secuencias.linaje_pango
-    IS 'Linaje pango de la secuencia genómica SARS-CoV-2 en GISAID';
+    IS 'Linaje pango de la secuencia genómica SARS-CoV-2';
+
+COMMENT ON COLUMN Secuencias.variante
+    IS 'Variante de la secuencia genómica SARS-CoV-2';
+
+COMMENT ON COLUMN Secuencias.estado
+    IS 'Variable que indica si la secuencia esta eliminada o no. 1: activo 0: eliminado';
 	
 --------------TABLA VARIANTES-----------------
 CREATE TABLE Variantes(
   id_variante SERIAL NOT NULL,
-  nomenclatura VARCHAR(20) NOT NULL,
+  nomenclatura VARCHAR(30) NOT NULL,
   linaje_pango TEXT[],
   sustituciones_spike TEXT[],
-  nombre VARCHAR(20),
+  nombre VARCHAR(30),
   color VARCHAR(10) NOT NULL,
   CONSTRAINT pk_id_variante PRIMARY KEY (id_variante)
 );
 
-ALTER TABLE Variantes OWNER TO postgres;
+ALTER TABLE Variantes OWNER TO rlnivuldeinkhw;
 
 COMMENT ON TABLE Variantes
 	IS 'Tabla donde se registran las variantes';
@@ -110,13 +118,13 @@ COMMENT ON COLUMN Variantes.color
 --------------TABLA ALGORITMOS-----------------
 CREATE TABLE Algoritmos(
   id_algoritmo SERIAL NOT NULL,
-  nombre VARCHAR(20) NOT NULL,
-  parametro INTEGER DEFAULT 0,
+  nombre VARCHAR(30) NOT NULL,
+  parametro DOUBLE PRECISION,
   algoritmo_entrenado BYTEA,
   CONSTRAINT pk_id_algoritmo PRIMARY KEY (id_algoritmo)
 );
 
-ALTER TABLE Algoritmos OWNER TO postgres;
+ALTER TABLE Algoritmos OWNER TO rlnivuldeinkhw;
 
 COMMENT ON TABLE Algoritmos
 	IS 'Tabla donde se registran los algoritmos';
@@ -158,7 +166,7 @@ ALTER TABLE ONLY Agrupamiento
 	FOREIGN KEY (id_variante) 
 	REFERENCES variantes(id_variante);
 
-ALTER TABLE Agrupamiento OWNER TO postgres;
+ALTER TABLE Agrupamiento OWNER TO rlnivuldeinkhw;
 
 COMMENT ON TABLE Agrupamiento
 	IS 'Tabla donde se registran los agrupamientos realizados';
@@ -181,13 +189,12 @@ COMMENT ON COLUMN Agrupamiento.num_cluster
 --------------TABLA ARCHIVOS-----------------
 CREATE TABLE Archivos(
   id_archivo SERIAL NOT NULL,
-  matriz_distancia BYTEA,
-  mds BYTEA,
-  pca BYTEA,
+  nombre	VARCHAR(30) NOT NULL,
+  archivo	BYTEA,
   CONSTRAINT pk_id_archivo PRIMARY KEY (id_archivo)
 );
 
-ALTER TABLE Archivos OWNER TO postgres;
+ALTER TABLE Archivos OWNER TO rlnivuldeinkhw;
 
 COMMENT ON TABLE Archivos
 	IS 'Tabla donde se registran los archivos';
@@ -195,11 +202,8 @@ COMMENT ON TABLE Archivos
 COMMENT ON COLUMN Archivos.id_archivo
     IS 'Identificador del registro en la tabla archivos';
 
-COMMENT ON COLUMN Archivos.matriz_distancia
-    IS 'Archivo que contiene la matriz de distancia';
+COMMENT ON COLUMN Archivos.nombre
+    IS 'Nombre del archivo';
 
-COMMENT ON COLUMN Archivos.mds
-    IS 'Archivo de MDS';
-
-COMMENT ON COLUMN Archivos.pca
-    IS 'Archivo de PCA';
+COMMENT ON COLUMN Archivos.archivo
+    IS 'Archivo que contiene la información';
